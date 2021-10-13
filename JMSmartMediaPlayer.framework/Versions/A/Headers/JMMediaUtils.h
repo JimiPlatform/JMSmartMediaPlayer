889c0a42035835ae050c1e60c8697827e35dd9cb
li.zhijian <lizhijian@jimilab.com>
2021-10-13 15:27:16 +0800

    v1.1.9

--- JMSmartMediaPlayer.framework/Versions/A/Headers/JMMediaUtils.h
+++ JMSmartMediaPlayer.framework/Versions/A/Headers/JMMediaUtils.h
@@ -14,6 +14,19 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (NSString *)CStringtoNSString:(const char *)errStr;
 
+/// 视频转码成H264+AAC数据流文件(请放置子线程)
+/// @param inFilePath 视频输入路径
+/// @param outFilePath 视频输出路径
++ (BOOL)startVideoConverter:(NSString *)inFilePath outFilePath:(NSString *)outFilePath;
+
+/// 视频转码成H264+AAC数据流文件（强制转码，请放置子线程）
+/// @param inFilePath 视频输入路径
+/// @param outFilePath 视频输出路径
++ (BOOL)startForceVideoConverter:(NSString *)inFilePath outFilePath:(NSString *)outFilePath;
+
+/// 中断视频转码
++ (void)interruptVideoConverter;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- JMSmartMediaPlayer.podspec
+++ JMSmartMediaPlayer.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = "JMSmartMediaPlayer"
-  s.version = "1.1.8"
+  s.version = "1.1.9"
   s.summary = "Jimi Meida Player SDK for iOS at CocoaPods."
   s.license = {"type"=>"Apache License 2.0", "file"=>"LICENSE"}
   s.authors = {"Jason"=>"lizhijian@jimilab.com"}
